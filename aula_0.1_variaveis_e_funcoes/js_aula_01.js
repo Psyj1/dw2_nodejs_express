441feffc11fa4cf5db31a0672730103151483446
Psyj1 <seiji.paulo@proton.me>
2025-02-12 21:07:16 -0300

    Primeiro commit

---
+++ aula_0.1_variaveis_e_funcoes/js_aula_01.js
@@ -0,0 +1,26 @@
+// Comentário e JS 
+
+/*
+
+nobari uana
+sias tchugeeeeeeruuuuuu
+marinomaaaaaaarinou...
+
+- akon, 2000s */
+
+//Declaração de Variáveis no JS
+//Const x Let x Var (o corinthians não gosta disso)
+//No escopo global, Const nao pode ser alterado
+
+const nome = "Akon"
+let Nome
+//const nome = "Pedro" -> resulta em um erro
+//Let Nome -> resulta em um erro
+
+var cor = "Azul"
+var cor = "Amarelo"
+let cidade = "Registro"
+cidade = "Jacupiranga" // Let permite alterar o valor
+
+const message = "Hello World!"
+console.log(message)
