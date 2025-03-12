ccea657a213b7b6ca6cf1d779d73915a8fc257e9
Psyj1 <seiji.paulo@proton.me>
2025-03-12 19:47:46 -0300

    Ordenação

--- aula_0,2_arrays_e_objetos/js_aula0.2_arrays_e_objetos.js
+++ aula_0,2_arrays_e_objetos/js_aula0.2_arrays_e_objetos.js
@@ -27,3 +27,46 @@ console.log("Showing the list in the forEach whit the variables")
 Products.forEach((Products, i) => {
     console.log(`${i+1} - ${Products}`)
 })
+
+// Métodos de manipulacao de vetores
+let vetor = ['Orange', 'Apple', 'Banana']
+console.log(`Our vector are: ${vetor}`)
+vetor[3] = 'Strawberry'
+console.log(`Our vector are: ${vetor} `)
+
+// Método PUSH - Insere um novo elemento no final do vetor
+vetor.push('Pineapple')
+console.log(`Our vector are: ${vetor} `)
+
+// Método UNSHIFT - Insere um novo elemento no ÍNICIO do vetor
+
+vetor[0] = 'Pear'
+console.log(`Our vector are: ${vetor} `)
+vetor.unshift('Orange')
+console.log(`Our vector are: ${vetor} `)
+
+// Método LENGTH - Retorna o número de elementos no vetor
+
+let numbers = [6, 8, 2, 9, 3, 200, 800]
+console.log(`Our list of numbers is: ${numbers}`)
+console.log(`The number of elements is: ${numbers.length} `)
+
+// Método SORT - Ordenar o vetor 
+
+console.log(`The first element of the list of fruits is: ${vetor[0]}`)
+vetor.sort()
+console.log(`Now the first element of the list is: ${vetor[0]} `)
+console.log(`Our list of fruits ordenaded is ${vetor.sort()}`)
+
+// Ordenação de números com SORT
+
+console.log(`Our list of numbers is ${numbers}`)
+numbers.sort()
+console.log(`The list of numbers ordenaded is: ${numbers}`)
+
+// Ordenação de forma CRESCENTE
+console.log(numbers.sort((a, b) => a - b))
+
+// Ordenação de forma DECRESCENTE
+console.log(numbers.sort((a, b) => b - a))
+
